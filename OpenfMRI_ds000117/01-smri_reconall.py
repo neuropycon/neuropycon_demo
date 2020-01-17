00a570174ea02858a6f2c3ad680702db8a31ffd1
annapasca <a.pascarella@iac.cnr.it>
2020-01-17 20:33:13 +0100

    add ephypype f for infosource and datagrabber

--- OpenfMRI_ds000117/01-smri_reconall.py
+++ OpenfMRI_ds000117/01-smri_reconall.py
@@ -12,13 +12,14 @@ Make sure that Freesurfer is properly configured before running this script.
 # Import modules
 import os
 import os.path as op
-import nipype.interfaces.io as nio
 import nipype.pipeline.engine as pe
 
-from nipype.interfaces.freesurfer import MRIConvert, ReconAll
-from nipype.interfaces.utility import IdentityInterface, Function
+from nipype.interfaces.freesurfer import ReconAll
+from nipype.interfaces.utility import Function
 
-from params import is_nii, subjects_dir, subject_ids, NJOBS
+from ephypype.nodes import create_iterator, create_datagrabber
+
+from params import subjects_dir, subject_ids, NJOBS
 from params import MRI_path, FS_WF_name, MAIN_WF_name
 
 
@@ -36,64 +37,20 @@ def create_main_workflow_FS_segmentation():
 
     print('SUBJECTS_DIR %s ' % os.environ["SUBJECTS_DIR"])
 
-    # (1) iterate over subjects to define paths with templates -> Infosource
-    #     and DataGrabber
-    #     Node: SubjectData - we use IdentityInterface to create our own node,
-    #     to specify the list of subjects the pipeline should be executed on
-    infosource = pe.Node(interface=IdentityInterface(fields=['subject_id']),
-                         name="infosource")
-    infosource.iterables = ('subject_id', subject_ids)
+    # (1) we create a node to pass input filenames to DataGrabber from nipype
+    #     iterate over subjects
+    infosource = create_iterator(['subject_id'], [subject_ids])
 
-    # Grab data
-    #   the template can be filled by other inputs
-    #   Here we define an input field for datagrabber called subject_id.
-    #   This is then used to set the template (see %s in the template).
-
-    # we can look for DICOM files or .nii ones
-    if is_nii:
-        datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
-                                                       outfields=['struct']),
-                             name='datasource')
-        datasource.inputs.template = '%s/anatomy/highres001.nii.gz'
-        datasource.inputs.template_args = dict(struct=[['subject_id']])
-    else:
-        datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
-                                                       outfields=['dcm_file']),
-                             name='datasource')
-        datasource.inputs.template = '%s/anatomy/*/*/*/*/*'
-        datasource.inputs.template_args = dict(dcm_file=[['subject_id']])
-
-    datasource.inputs.base_directory = MRI_path  # dir where the MRI files are
-    datasource.inputs.sort_filelist = True
-
-    # get the path of the first dicom file
-    def get_first_file(dcm_files):
-        return dcm_files[0]
-
-    # return the path of the struct filename in the MRI sbj dir that will be
-    # the  input of MRI convert routine
-    def get_MRI_sbj_dir(dcm_file):
-        from nipype.utils.filemanip import split_filename as split_f
-        import os.path as op
-
-        MRI_sbj_dir, basename, ext = split_f(dcm_file)
-        struct_filename = op.join(MRI_sbj_dir, 'struct.nii.gz')
-        return struct_filename
-
-    get_firstfile = pe.Node(interface=Function(input_names=['dcm_files'],
-                                               output_names=['dcm_file'],
-                            function=get_first_file), name='get_firstfile')
-
-    get_MRI_sbjdir = pe.Node(interface=Function(input_names=['dcm_file'],
-                                                output_names=['struct_filename'],  # noqa
-                             function=get_MRI_sbj_dir), name='get_MRI_sbjdir')
-
-    # MRI_convert Node
-    # We use it if we don't have a .nii.gz file
-    # The output of mriconvert is the input of recon-all
-    mri_convert = pe.Node(interface=MRIConvert(), infields=['in_file'],
-                          outfields=['out_file'],
-                          name='mri_convert')
+    # # and a node to grab data. The template_args in this node iterate upon
+    # the values in the infosource node
+    # Here we define an input field for datagrabber called subject_id.
+    # This is then used to set the template (see %s in the template).
+    # we look for .nii files
+    template_path = '%s/anatomy/highres001.nii.gz'
+    template_args = [['subject_id']]
+    infields = ['subject_id']
+    datasource = create_datagrabber(MRI_path, template_path, template_args,
+                                    infields=infields)
 
     # (2) ReconAll Node to generate surfaces and parcellations of structural
     #     data from anatomical images of a subject.
@@ -104,7 +61,6 @@ def create_main_workflow_FS_segmentation():
 
     # reconall_workflow will be a node of the main workflow
     reconall_workflow = pe.Workflow(name=FS_WF_name)
-
     reconall_workflow.base_dir = MRI_path
 
     reconall_workflow.connect(infosource, 'subject_id',
@@ -113,21 +69,7 @@ def create_main_workflow_FS_segmentation():
     reconall_workflow.connect(infosource, 'subject_id',
                               datasource,  'subject_id')
 
-    if is_nii:
-        reconall_workflow.connect(datasource, 'struct', recon_all, 'T1_files')
-    else:
-        reconall_workflow.connect(datasource,   'dcm_file',
-                                  get_firstfile,  'dcm_files')
-        reconall_workflow.connect(get_firstfile, 'dcm_file',
-                                  get_MRI_sbjdir, 'dcm_file')
-
-        reconall_workflow.connect(get_firstfile, 'dcm_file',
-                                  mri_convert, 'in_file')
-        reconall_workflow.connect(get_MRI_sbjdir, 'struct_filename',
-                                  mri_convert, 'out_file')
-
-        reconall_workflow.connect(mri_convert, 'out_file',
-                                  recon_all, 'T1_files')
+    reconall_workflow.connect(datasource, 'raw_file', recon_all, 'T1_files')
 
     # (3) BEM generation by make_watershed_bem of MNE Python package
     main_workflow = pe.Workflow(name=MAIN_WF_name)
@@ -200,5 +142,4 @@ if __name__ == '__main__':
 
     main_workflow.write_graph(graph2use='colored')
     main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
-    main_workflow.run(plugin='LegacyMultiProc',
-                      plugin_args={'n_procs': NJOBS})
+    main_workflow.run(plugin='LegacyMultiProc', plugin_args={'n_procs': NJOBS})
