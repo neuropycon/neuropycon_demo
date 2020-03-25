3f4ea1044bdf5fe65e5efb074f78d3d2a8c50077
David Meunier <david_meunier_79@hotmail.fr>
2020-03-25 16:13:34 +0100

    hotfix : bug in fetch_data

--- OpenfMRI_ds000117/00-fetch_data.py
+++ OpenfMRI_ds000117/00-fetch_data.py
@@ -7,51 +7,66 @@ This script gives some basic code that can be adapted to fetch data.
 """
 
 import os
-from params import subjects_dir, data_path
+import json
 
-pwd = os.getcwd()
+#from params import subjects_dir, data_path
+def fetch_data_demo(data_path = ""):
+    if data_path == "":
+        data_path = os.path.expanduser("")
 
-print("data_path : %s" % data_path)
+    data_path = os.path.join(data_path, "data_demo")
 
-if not os.path.exists(data_path):
-    data_path = os.path.abspath("data_demo")
-    try:
-        os.mkdir(data_path)
-    except FileExistsError:
-        print ('data_demo already exists')
+    print("data_path : %s" % data_path)
 
-os.chdir(data_path)
+    if not os.path.exists(data_path):
+        try:
+            os.mkdir(data_path)
+        except FileExistsError:
+            print ('data_demo already exists')
 
-archive_dir = os.path.join(os.getcwd(), 'archive')
+    os.chdir(data_path)
 
-if not os.path.isdir(archive_dir):
-    os.mkdir(archive_dir)
+    #os.system('wget http://openfmri.s3.amazonaws.com/tarballs/ds117_R0.1.1_metadata.tgz')  # noqa: E501
+    #os.system('tar xvzf ds117_R0.1.1_metadata.tgz')
+    #os.system('mkdir metadata')
+    #os.chdir(os.path.join(data_path, 'ds117'))
+    #os.system('mv stimuli study_key.txt models README scan_key.txt model_key.txt listing.txt license.txt emptyroom ../metadata/')  # noqa: E501
+    #os.chdir(data_path)
+    #os.system('rmdir ds117')
 
-os.system('wget http://openfmri.s3.amazonaws.com/tarballs/ds117_R0.1.1_metadata.tgz')  # noqa: E501
-os.system('tar xvzf ds117_R0.1.1_metadata.tgz')
-os.system('mkdir metadata')
-os.chdir(os.path.join(data_path, 'ds117'))
-os.system('mv stimuli study_key.txt models README scan_key.txt model_key.txt listing.txt license.txt emptyroom ../metadata/')  # noqa: E501
-os.chdir(data_path)
-os.system('rmdir ds117')
-os.system('mv ds117_R0.1.1_metadata.tgz archive/')
+    #downloding subject data
+    for subject in subject_ids:
+        print("processing %s" % subject)
+        fname = "ds117_R0.1.1_%s_raw.tgz" % subject
+        url = "http://openfmri.s3.amazonaws.com/tarballs/" + fname
+        if os.path.isdir(subject):
+            continue
+        if not os.path.exists(fname):
+            os.system('wget %s' % url)
+        os.system('tar xvzf %s' % fname)
+        os.system('mv ds117/%s .' % subject)
+        #os.system('mv %s archive/' % fname)
+        os.system('rmdir ds117')
+
+    subjects_dir = os.path.join(data_path, subjects_dir)
+
+    if not os.path.isdir(subjects_dir):
+        os.mkdir(subjects_dir)
 
 
-for i in range(1, 20):
-    subject = "sub%03d" % i
-    print("processing %s" % subject)
-    fname = "ds117_R0.1.1_%s_raw.tgz" % subject
-    url = "http://openfmri.s3.amazonaws.com/tarballs/" + fname
-    if os.path.isdir(subject):
-        continue
-    if not os.path.exists(fname):
-        os.system('wget %s' % url)
-    os.system('tar xvzf %s' % fname)
-    os.system('mv ds117/%s .' % subject)
-    os.system('mv %s archive/' % fname)
-    os.system('rmdir ds117')
+rel_path = os.path.split(os.path.realpath(__file__))[0]
+print(rel_path)
+params = json.load(open(os.path.join(rel_path, "params.json")))
 
-os.chdir(pwd)
+print(params)
+subjects_dir = params["subjects_dir"]
+subject_ids = params["subject_ids"]
 
-if not os.path.isdir(subjects_dir):
-    os.mkdir(subjects_dir)
+if "data_path" in params.keys():
+    data_path = params["data_path"]
+else:
+    data_path = ""
+
+print (data_path, subjects_dir, subject_ids)
+
+fetch_data_demo(data_path, subjects_dir, subject_ids)
